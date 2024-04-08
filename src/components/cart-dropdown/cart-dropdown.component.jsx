302456d6ef8317e0fef164d9fd84533291c9c0da
Rajesh Cherla <rajesh.cherla@centvis.com>
2024-04-08 17:42:18 +0530

    feat: Shopping Cart

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,11 +1,14 @@
 import Button from '../button/button.component';
+import CartItem from '../cart-item/cart-item.component';
 
 import './cart-dropdown.styles.scss';
 
 const CartDropDown = () => {
     return (
         <div className='cart-dropdown-container'>
-            <div className='cart-items'></div>
+            <div className='cart-items'>
+                {[].map(item => <CartItem cartItems={item} />)}
+            </div>
             <Button>Checkout</Button>
         </div>
     );
