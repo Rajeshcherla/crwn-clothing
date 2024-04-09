3c6528c42b165bf9a9c5b829de3ed884ed6b0777
Mystic force <rajeshcherla@gmail.com>
2024-04-09 23:36:13 +0530

    feat: shopping cart items

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,13 +1,16 @@
+import { useContext } from 'react';
 import Button from '../button/button.component';
 import CartItem from '../cart-item/cart-item.component';
 
 import './cart-dropdown.styles.scss';
+import { CartContext } from '../../context/cart.context';
 
 const CartDropDown = () => {
+    const { cartItems } = useContext(CartContext)
     return (
         <div className='cart-dropdown-container'>
             <div className='cart-items'>
-                {[].map(item => <CartItem cartItems={item} />)}
+                {cartItems.map(item => <CartItem key={item.id} cartItems={item} />)}
             </div>
             <Button>Checkout</Button>
         </div>
