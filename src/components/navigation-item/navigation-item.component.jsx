3954f3ffe13191c69c676216e18b8af8808d3359
Mystic force <rajeshcherla@gmail.com>
2024-04-11 11:11:05 +0530

    feat: home page routing

--- src/components/navigation-item/navigation-item.component.jsx
+++ src/components/navigation-item/navigation-item.component.jsx
@@ -1,9 +1,14 @@
-import './navigation-item.style.scss'
+import { useNavigate } from 'react-router-dom';
+import './navigation-item.style.scss';
 
 const NavigationItem = ({ category }) => {
-    const { imageUrl, title } = category;
+    const { imageUrl, title, route } = category;
+    const navigate = useNavigate();
+
+    const onNavigateHandler = () => navigate(route);
+
     return (
-        <div className="navigation-item-container">
+        <div className="navigation-item-container" onClick={onNavigateHandler}>
             <div
                 className="background-image"
                 style={{
